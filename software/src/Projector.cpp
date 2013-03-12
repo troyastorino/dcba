451d71d2ae82893da8ba6cf1e72c0e2cf5948047
Turner Bohlen <turnerbohlen@gmail.com>
2013-03-11 23:57:53 -0400

    Adds Projector.cpp file

---
+++ software/src/Projector.cpp
@@ -0,0 +1,12 @@
+#include "Projector.hpp"
+
+using namespace std;
+
+GeneratedPattern* Projector::projectPattern( const Projector* projector, const Pattern* pattern ) {
+  // checks to make sure that the projector can project the given pattern
+
+  // creates a new matrix and distorts the pattern according to the projector
+  // parameters
+
+  // wraps that new image in a GeneratedPattern and returns it
+}
