af16059a0c42dcf284ed0ecd99824b38e4c61fc4
Troy Astorino <troyastorino@gmail.com>
2013-03-19 17:10:24 -0400

    Fixed docstring issue, added image loading function, some test images

---
+++ materials/test-images/swept-plane/man/man_v1.m
@@ -0,0 +1,27 @@
+
+% Specify the object and reconstruction parameters.
+objName   = 'man';         % object name (should correspond to a data dir.)
+calName   = 'calib';       % calibration source (also must be a data dir.)
+seqName   = 'v1';          % sequence name (subdirectory of object)
+allFrames = 1:200;         % all frames in this sequence
+recFrames = 61:151;        % frames used for reconstruction
+
+% Set reconstruction parameters.
+useLowRes   = true;         % enables low resolution imagery (for debugging)
+minContrast = 50;           % pixel exclusion threshold (min. contrast)
+dSample     = 5;            % down-sampling rate for final point cloud
+distReject  = 20;           % rejection distance (distance in mm for outlier removal)
+
+% Define "horizonal" and "vertical" reference plane properties.
+% Note: These lengths are used to determine the extrinsic calibration from 
+%       the distances between printed fiducials on each foamcore plane.
+nX = 1;        % number of rectangles along x-axis
+nY = 1;        % number of rectangles along y-axis
+dX = 558.8;    % length along x-axis (mm) (between checkerboard rectangles)
+dY = 303.2125; % length along y-axis (mm) (between checkerboard rectangles)
+
+% Define bounding volume to clip final results.
+% Note: This eliminates outliers and crops foamcore boards from 3D model.
+clipRangeX = [5 dX-5];  % clip range along x-axis (mm)
+clipRangeY = [5 dY-5];  % clip range along y-axis (mm)
+clipRangeZ = [5 dY+50]; % clip range along z-axis (mm)
--- software/src/data_capture/image.py
+++ software/src/data_capture/image.py
@@ -1,19 +1,47 @@
+import os
+import cv2
+
 class Image:
-"""
-Class: Image
+    """
+    Class: Image
+    Stores all the information relevant to one captured image from the
+    environment. This includes the patterns projected on the object when the
+    image was taken, the camera that took the image, and the image data itself,
+    in matrix form.
 
- Stores all the information relevant to one captured image from the
- environment. This includes the patterns projected on the object when the
- image was taken, the camera that took the image, and the image data itself,
- in matrix form.
-
- Properties:
- Patterns - list of <GeneratedPattern>s; light patterns projected on
- the object when the image was taken
- camera - The <Camera> that took the image
- data - ndarray The pixel data comprising the image
-"""
+    Properties:
+    Patterns - list of <GeneratedPattern>s; light patterns projected on
+    the object when the image was taken
+    camera - The <Camera> that took the image
+    data - ndarray The pixel data comprising the image
+    """
     def __init__(self, data, camera, patterns):
         self.data = data
         self.camera = camera
         self.patterns = patterns
+
+def load_from_directory(dir_name):
+    """
+    Function: load_from_directory
+    Loads all of the image files in a directory and returns them as a list of <Image>s
+
+    Parameters:
+    string dir_name - The name of the directory
+
+    Returns:
+    A list of <Image>s read from the directory
+
+    """
+    # list of images to return
+    images = []
+
+    # try to add each item in the directory as an image
+    for f in os.listdir(dir_name):
+        data = cv2.imread(os.path.join(dir_name, f))
+
+        # if data collected, add the Image
+        if data != None:
+            images.append(Image(data, None, None))
+
+    # return the list of images
+    return images
