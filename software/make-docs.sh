cc219f66d7aac68a0bbb879a58fd0a8d009e0371
Troy Astorino <troyastorino@gmail.com>
2013-04-13 01:28:46 -0400

    Fixing up some documentation issues

--- software/make-docs.sh
+++ software/make-docs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 type dos2unix >/dev/null 2>&1 || { echo >&2 "We need dos2unix to create the documentation. Please install it and then run this script again"; exit 1; }
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-INCLUDES="${DIR}/include"
+LIB_DIR="${DIR}/lib"
 PYTHON_SRC="${DIR}/python/scan"
+LIBS=""
+LIBS="${LIBS} -i ${LIB_DIR}/python-pcl"
+echo $LIBS
 PYTHON_FOLDERS=""
 for FOLDER in `ls ${PYTHON_SRC}`
 do
@@ -11,7 +14,7 @@ done
 OUTPUT="${DIR}/docs"
 mkdir -p $OUTPUT
 PROJECT="${DIR}/lib/natural-docs/data"
-./lib/natural-docs/NaturalDocs -i $PYTHON_SRC -xi ${DIR}/lib -o framedHTML $OUTPUT -p $PROJECT $*
+./lib/natural-docs/NaturalDocs -i $PYTHON_SRC $LIBS -xi ${LIB_DIR}/natural-docs -o framedHTML $OUTPUT -p $PROJECT $*
 dos2unix "${DIR}/docs/javascript/main.js"
 dos2unix "${DIR}/docs/styles/main.css"
 echo "Done!"
